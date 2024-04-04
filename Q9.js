a8e6200e8cb8a793dc67eeaac9f798a9fa385459
Deepjay96 <jaydipgirase65904@gmail.com>
2024-04-04 23:13:21 +0530

    avg num

---
+++ Q9.js
@@ -0,0 +1,21 @@
+// write a function it takes array as an input and return average of those  numbers which are in array  
+
+
+function avgNumbers(a){
+      let num = 0
+      let avg ;
+      for(let i=0;i<a.length;i++){
+          num = num + a[i]
+      }
+  
+      return  avg = (num/a.length)
+    
+}
+
+console.log(avgNumbers([10,3,5,6,1]))
+
+
+
+
+
+
